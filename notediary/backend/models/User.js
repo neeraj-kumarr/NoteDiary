7c894ddea12963d9f057507e3c79bceb4118291e
Neeraj Kumar <neeraj.dsu@gmail.com>
2024-05-02 18:02:15 +0500

    Created Modals and Routes

---
+++ notediary/backend/models/User.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    tag: {
+        type: String,
+        default: 'General'
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('user', userSchema)
