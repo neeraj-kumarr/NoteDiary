bf178a9f0c2a4d74686f39f717f29485dfa78eb8
Neeraj Kumar <neeraj.dsu@gmail.com>
2024-05-10 13:49:03 +0500

    Adding express-validator

--- notediary/backend/routes/auth.js
+++ notediary/backend/routes/auth.js
@@ -1,14 +1,45 @@
 const express = require('express');
 const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
 const router = express.Router();
 
-// // Create a User using: POST "/api/auth/". Doesn't require Auth
-router.post('/', (req, res) => {
 
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
-})
+const validate = validations => {
+    return async (req, res, next) => {
+        // loop runs against each request
+        for (let validation of validations) {
+            const result = await validation.run(req);
+            if (result.errors.length) break;
+        }
+
+        const errors = validationResult(req);
+        if (errors.isEmpty()) {
+            return next();
+        }
+
+        res.status(400).json({ errors: errors.array() });
+    };
+};
+
+
+// // Create a User using: POST "/api/auth/". Doesn't require Auth
+router.post('/', validate([
+    body('name', 'Name length must be atleast 3 characters').isLength({ min: 3 }).withMessage('Name length must be atleast 5 characters'),
+    body('password').isLength({ min: 5 }).withMessage('Password length must be atleast 5 characters'),
+    body('email').isEmail().withMessage('Not a valid e-mail address'),
+]), async (req, res) => {
+
+    try {
+        const { name, password, email } = req.body;
+
+        await User.create({ name, password, email });
+
+        res.send({ name, email }); // Sending back only necessary information
+    } catch (error) {
+        console.error('Error creating user:', error);
+        res.json({ error: 'Email already in use', message: error.message });
+    }
+
+});
 
 module.exports = router;
